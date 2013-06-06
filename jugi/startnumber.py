f49a0cb242f22840015fa7bfa9c9b21635138c34
Andreas Stricker <andy@knitter.ch>
2013-06-07 01:17:25 +0200

    Add startnumber assignment code

---
+++ jugi/startnumber.py
@@ -0,0 +1,16 @@
+import jugi.db
+
+def assign_start_number_from_id(db):
+    db.store.execute('UPDATE Athlete SET number = id_athlete')
+
+def assign_start_number_sequential(db, start=0, skip_assigned=True):
+    if skip_assigned:
+        result = db.store.execute('SELECT MAX(number) FROM Athlete')
+        start = max(result.get_one()[0], start)
+    n = start
+    for athlete in db.store.find(jugi.db.Athlete):
+        n += 1
+        if skip_assigned and (athlete.number > 0):
+            continue
+        athlete.number = n
+    db.store.commit()
