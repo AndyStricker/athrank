4960439b0a4b7fb0a9669d5731cd3feec508739d
Andreas Stricker <andy@knitter.ch>
2013-06-09 14:57:05 +0200

    Working start number assignment with script

--- athrank/startnumber.py
+++ athrank/startnumber.py
@@ -2,11 +2,14 @@ import athrank.db
 
 def assign_start_number_from_id(db):
     db.store.execute('UPDATE Athlete SET number = id_athlete')
+    db.store.commit()
 
 def assign_start_number_sequential(db, start=0, skip_assigned=True):
     if skip_assigned:
         result = db.store.execute('SELECT MAX(number) FROM Athlete')
         start = max(result.get_one()[0], start)
+    else:
+        db.store.execute('UPDATE Athlete SET number = NULL')
     n = start
     for athlete in db.store.find(athrank.db.Athlete):
         n += 1
