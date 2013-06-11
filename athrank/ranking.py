ae8ae81405acbc41c9bf21a6520ae6ecd782c09a
Andreas Stricker <andy@knitter.ch>
2013-06-11 23:54:42 +0200

    Add cut-off values in rankings

--- athrank/ranking.py
+++ athrank/ranking.py
@@ -108,28 +108,38 @@ class ScoreTable94(object):
 
     def sprint(self, category, value):
         if category in ('MJ', 'MA'):   # 100m
+            if value >= 21.43: return 1
             return int(7.89305 * (21.8 - value) ** 2.1)
         elif category in ('MB'):       # 80m
+            if value >= 17.73: return 1
             return int(11.754907 * (18.03 - value) ** 2.1)
         elif category in ('MC', 'MD'): # 60m
+            if value >= 13.93: return 1
             return int(19.742424 * (14.17 - value) ** 2.1)
         elif category in ('ME', 'MF'): # 50m
+            if value >= 12.15: return 1
             return int(26.011098 * (12.36 - value) ** 2.1)
         elif category in ('KJ', 'KA'): # 100m
+            if value >= 21.11: return 1
             return int(7.080303 * (21.5 - value) ** 2.1)
         elif category in ('KB'):       # 80m
+            if value >= 17.46: return 1
             return int(10.54596 * (17.78 - value) ** 2.1)
         elif category in ('KC', 'KD'): # 60m
+            if value >= 13.72: return 1
             return int(17.686955 * (13.97 - value) ** 2.1)
         elif category in ('KE', 'KF'): # 50m
+            if value >= 11.97: return 1
             return int(23.327251 * (12.19 - value) ** 2.1)
         else:
             raise Exception("Invalid category %s" % category)
 
     def endurancerun(self, category, value):
         if category in ('MJ', 'MA', 'MB', 'MC'):
+            if value > 332.89: return 1
             return int(0.006914 * (341.58 - value) ** 2.3)
         elif category in ('KJ', 'KA', 'KB', 'KC'):
+            if value >= 317.07: return 1
             return int(0.0068251 * (325.81 - value) ** 2.3)
         elif category in ('MD', 'ME', 'MF', 'KD', 'KE', 'KF'): # no endurance run
             return 0
@@ -137,16 +147,20 @@ class ScoreTable94(object):
 
     def longjump(self, category, value):
         if category in ('MJ', 'MA', 'MB', 'MC', 'MD', 'ME', 'MF'):
+            if value <= 1.8: return 1
             return int(220.628792 * (value - 1.8) ** 1.0)
         elif category in ('KJ', 'KA', 'KB', 'KC', 'KD', 'KE', 'KF'):
+            if value <= 1.9: return 1
             return int(180.85908 * (value - 1.9) ** 1.0)
         else:
             raise Exception("Invalid category %s" % category)
 
     def highjump(self, category, value):
         if category in ('MJ', 'MA', 'MB', 'MC'):
+            if value <= 0.62: return 1
             return int(855.310049 * (value - 0.62) ** 1.0)
         elif category in ('KJ', 'KA', 'KB', 'KC'):
+            if value <= 0.65: return 1
             return int(690.05175 * (value - 0.65) ** 1.0)
         elif category in ('MD', 'ME', 'MF', 'KD', 'KE', 'KF'): # no highjump
             return 0
@@ -155,8 +169,10 @@ class ScoreTable94(object):
 
     def shotput(self, category, value):
         if category in ('MJ', 'MA', 'MB'):
+            if value <= 1.3: return 1
             return int(83.435373 * (value - 1.3) ** 0.9)
         elif category in ('KJ', 'KA', 'KB', 'KC'):
+            if value <= 1.78: return 1
             return int(82.491673 * (value - 1.78) ** 0.9)
         elif category in ('MC', 'MD', 'ME', 'MF', 'KD', 'KE', 'KF'):
             return 0
@@ -165,8 +181,10 @@ class ScoreTable94(object):
 
     def ball(self, category, value):
         if category in ('MC', 'MD', 'ME', 'MF'):
+            if value <= 5.03: return 1
             return int(22.0 * (value - 5) ** 0.9)
         elif category in ('KD', 'KE', 'KF'):
+            if value <= 8.04: return 1
             return int(18.0 * (value - 8) ** 0.9)
         elif category in ('MJ', 'MA', 'MB', 'KJ', 'KA', 'KB', 'KC'): # shotput
             return 0
